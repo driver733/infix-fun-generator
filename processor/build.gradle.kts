39c74f9537f7d8f5768f9143563af459f5c0a751
Mikhail Yakushin <driver733@gmail.com>
2020-05-05 19:44:30 +0300

    feat: infix fun generation

---
+++ processor/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.driver733.gradle-kotlin-setup-plugin")
+    `maven-publish`
+}
+
+dependencies {
+    implementation(project(":annotations"))
+
+    implementation("com.squareup:kotlinpoet:1.5.0")
+    implementation("com.google.auto.service:auto-service:1.0-rc6")
+    kapt("com.google.auto.service:auto-service:1.0-rc6")
+}
