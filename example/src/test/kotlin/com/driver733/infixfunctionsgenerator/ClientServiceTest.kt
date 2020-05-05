ce9815ac8b5afaabf735127190e36a2b1dfb519b
Mikhail Yakushin <driver733@gmail.com>
2020-05-05 19:44:30 +0300

    feature: infix fun generation

---
+++ example/src/test/kotlin/com/driver733/infixfunctionsgenerator/ClientServiceTest.kt
@@ -0,0 +1,47 @@
+package com.driver733.infixfunctionsgenerator
+
+import com.memoizr.assertk.assert
+import createWithName
+import deleteByName
+import findByName
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
+
+object ClientServiceTest : Spek({
+
+    Feature("Infix functions generation") {
+        val clients = ClientService()
+
+        Scenario("fun with one param") {
+            lateinit var actual: String
+            When("infix fun is called") {
+                actual = clients findByName "Alice"
+            }
+            Then("infix fun result is equal to the normal fun call") {
+                assert that actual isEqualTo clients.findBy("Alice")
+            }
+        }
+
+        Scenario("fun with two params") {
+            lateinit var actual: String
+            When("infix fun is called") {
+                actual = clients deleteByName "Bob" andAge 20
+            }
+            Then("infix fun result is equal to the normal fun call") {
+                assert that actual isEqualTo clients.deleteBy("Bob", 20)
+            }
+        }
+
+        Scenario("function with two params") {
+            lateinit var actual: String
+            When("infix fun is call") {
+                actual = clients createWithName "Carl" age 25 andHeight 170
+            }
+            Then("infix fun result is equal to the normal fun call") {
+                assert that actual isEqualTo clients.createBy("Carl", 25, 170)
+            }
+        }
+
+    }
+
+})
