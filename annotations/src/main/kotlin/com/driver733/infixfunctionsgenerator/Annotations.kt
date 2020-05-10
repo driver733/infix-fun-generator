c0b5b7af86f2bd003a5eb3943a8b25d3faad1c09
Mikhail Yakushin <driver733@gmail.com>
2020-05-11 00:11:42 +0300

    fix(#19): rename "@GenerateInfix" to "@Infix"
    BREAKING CHANGE: annotation name is changed

--- annotations/src/main/kotlin/com/driver733/infixfunctionsgenerator/Annotations.kt
+++ annotations/src/main/kotlin/com/driver733/infixfunctionsgenerator/Annotations.kt
@@ -2,4 +2,4 @@ package com.driver733.infixfunctionsgenerator
 
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.FUNCTION)
-annotation class GenerateInfix(val firstMethodName: String = "", vararg val intermediateMethodsNames: String = [])
+annotation class Infix(val firstMethodName: String = "", vararg val intermediateMethodsNames: String = [])
